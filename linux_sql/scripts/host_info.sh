1c554de2c952a14d523e3c1326ce9fa21aa548cc
Kanty Gakima <kantylouange.gakima@gmail.com>
2025-11-14 20:44:15 +0000

    little scripts modifs

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -6,7 +6,7 @@ psql_user=$4
 psql_password=$5
 
 # check the number of arguments
-if ["$#" -ne 5]; then
+if [ "$#" -ne 5 ]; then
   echo "Illegal number of parameters"
   exit 1
 fi
@@ -16,11 +16,13 @@ specs=`lscpu`
 hostname=$(hostname -f)
 
 # Retrieve hardware specification variables
-cpu_number=$(echo "$specs" | egrep "^CPU\(S\):" | awk '{print $2}' | xargs)
+cpu_number=$(echo "$specs" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$specs"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
-cpu_model= $(echo "$specs" | awk -F': +' '/^Model name:/ {print $2}' | xargs)
+cpu_model=$(echo "$specs" | awk -F': +' '/^Model name:/ {print $2}' | xargs)
 cpu_mhz=$(awk -F': +' '/^cpu MHz/ {print $2}' /proc/cpuinfo | uniq)
-l2_cache=$(echo "specs" | awk -F': +' '/^L2 cache:/ {print $2}' | xargs)
+#to do : change L2 cache to MB
+l2_cache=$(echo "$specs" | awk -F': +' '/^L2 cache:/ {print $2}' | awk '{print $1}' | sed 's/[^`0-9]//g' | xargs)
+
 total_mem=$(vmstat --unit M | awk '{print $4}' | tail -n1 | xargs)
 timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1 | xargs)
 
@@ -30,12 +32,12 @@ timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1 | xargs)
 insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache,"timestamp",
               total_mem)
               VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache',
-              '$timestamp, '$total_mem)";
+              '$timestamp', '$total_mem')";
 
 # set up env var for psql command
 export PGPASSWORD=$psql_password
 
 #insert data into the database
-psql -h $psql_host -p psql_port -d $db_name -U $psq_user -c "$insert_stmt"
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 
 exit $?
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -6,7 +6,7 @@ psql_user=$4
 psql_password=$5
 
 # check the number of arguments
-if ["$#" -ne 5]; then
+if [ "$#" -ne 5 ]; then
   echo "Illegal number of parameters"
   exit 1
 fi
@@ -16,23 +16,23 @@ vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
 
 # Retrieve hardware specification variables
-memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
-cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs )
-cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
-disk_io=$(vmstat -d | awk '{print $11}' | tail -n1 | xargs  )
-disk_available=$(df -BM / | awk '{print $4}' | tail -n1 | xargs)
-timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1 | xargs)
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1)
+cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1)
+disk_io=$(vmstat -d | awk '{print $11}' | tail -n1)
+disk_available=$(df -BM / | awk '{print $4}'| sed 's/M//' | tail -n1 )
+timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1)
 
 # Subquery to find maching id in host_info table
-host_id="(SELECT id fROM host_info WHERE hostname='$hostname')";
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 
 #PSQL command to insert server usage data into host_usage table
 insert_stmt="INSERT INTO host_usage("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-              VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle, '$cpu_kernel, ,$disk_io', '$disk_available')";
-# set up env var for psql command
+              VALUES('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernel ,$disk_io, $disk_available)";
+# set up env var for psql command'
 export PGPASSWORD=$psql_password
 
 #insert data into the database
-psql -h $psql_host -p psql_port -d $db_name -U $psq_user -c "$insert_stmt"
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 
 exit $?
