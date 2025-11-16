4985276118ede0d6a142e50402bf6821948f5fe3
Kanty Gakima <kantylouange.gakima@gmail.com>
2025-11-16 21:58:50 +0000

    modifs insert values

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,4 +1,5 @@
 
+#Capture CLI commands
 psql_host=$1
 psql_port=$2
 db_name=$3
@@ -20,16 +21,17 @@ memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1)
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1)
 disk_io=$(vmstat -d | awk '{print $11}' | tail -n1)
-disk_available=$(df -BM / | awk '{print $4}'| sed 's/M//' | tail -n1 )
+disk_available=$(df -BM / | awk '{print $4}'| sed 's/M//' | tail -n1 ) #sed 's/M//'  for parsing the M as we ony need int
 timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1)
 
-# Subquery to find maching id in host_info table
+# Subquery to find machine id in host_info table
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 
 #PSQL command to insert server usage data into host_usage table
 insert_stmt="INSERT INTO host_usage("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
               VALUES('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernel ,$disk_io, $disk_available)";
-# set up env var for psql command'
+
+# provide password to psql using env variable
 export PGPASSWORD=$psql_password
 
 #insert data into the database
