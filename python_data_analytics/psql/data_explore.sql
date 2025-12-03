685101b1e2047bc08f1523e8411465db2b713b59
Kanty Gakima <kantylouange.gakima@gmail.com>
2025-12-03 20:22:17 +0000

    init data_explore.sql

---
+++ python_data_analytics/psql/data_explore.sql
@@ -0,0 +1,12 @@
+-- Show table schema
+\d+ retail;
+
+-- SHow first 10 rows
+SELECT * FROM retail limit 10;
+
+--Check # of records
+Your query goes here
+
+-- Number of clients (e.g unique client IS)
+--YOUR QUERY Goes HERE
+
